586863fdfcb5cc960a25aaf486a249393b4e38d9
Jonas Keinholz <git@inyono.dev>
2020-12-22 15:41:29 +0100

    use correct release tag format

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -13,42 +13,42 @@ exec().then(() => {
 async function exec(): Promise<void> {
   const content = await generateChangelog<undefined>([
     {
-      tagName: '0.1.0',
+      tagName: 'v0.1.0',
       date: '2019-01-20',
       description: 'Initial working release',
     },
     {
-      tagName: '0.1.1',
+      tagName: 'v0.1.1',
       date: '2019-02-02',
       added: ['Add section `Changed`'],
     },
     {
-      tagName: '0.1.2',
+      tagName: 'v0.1.2',
       date: '2019-02-08',
       fixed: ['Move `@types/mdast` to dependencies'],
     },
     {
-      tagName: '0.1.3',
+      tagName: 'v0.1.3',
       date: '2019-06-05',
       added: ['Add scoped changelog entries'],
     },
     {
-      tagName: '0.1.4',
+      tagName: 'v0.1.4',
       date: '2019-06-05',
       fixed: ['Handle remote urls without optional .git'],
     },
     {
-      tagName: '0.1.5',
+      tagName: 'v0.1.5',
       date: '2020-02-14',
       fixed: ['Update dependencies to support Node v12'],
     },
     {
-      tagName: '0.2.0',
+      tagName: 'v0.2.0',
       date: '2020-12-22',
       yanked: true,
     },
     {
-      tagName: '0.2.1',
+      tagName: 'v0.2.1',
       date: '2020-12-22',
       breakingChanges: ['Drop Node v10 support.'],
       added: ['Update dependencies to support Node v14.'],
