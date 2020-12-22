51f1093cad0bb71d5e93797806016ffe8a27abc4
Jonas Keinholz <git@inyono.dev>
2020-12-22 14:41:37 +0100

    release v0.2.0

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as util from 'util'
 
-import { generateChangelog } from '..'
+import { generateChangelog } from '../src'
 
 const writeFile = util.promisify(fs.writeFile)
 
@@ -15,33 +15,33 @@ async function exec(): Promise<void> {
     {
       tagName: '0.1.0',
       date: '2019-01-20',
-      description: 'Initial working release'
+      description: 'Initial working release',
     },
     {
       tagName: '0.1.1',
       date: '2019-02-02',
-      added: ['Add section `Changed`']
+      added: ['Add section `Changed`'],
     },
     {
       tagName: '0.1.2',
       date: '2019-02-08',
-      fixed: ['Move `@types/mdast` to dependencies']
+      fixed: ['Move `@types/mdast` to dependencies'],
     },
     {
       tagName: '0.1.3',
       date: '2019-06-05',
-      added: ['Add scoped changelog entries']
+      added: ['Add scoped changelog entries'],
     },
     {
       tagName: '0.1.4',
       date: '2019-06-05',
-      fixed: ['Handle remote urls without optional .git']
+      fixed: ['Handle remote urls without optional .git'],
     },
     {
       tagName: '0.1.5',
       date: '2020-02-14',
-      fixed: ['Update dependencies to support Node v12']
-    }
+      fixed: ['Update dependencies to support Node v12'],
+    },
   ])
 
   await writeFile(path.join(__dirname, '..', 'CHANGELOG.md'), content)
